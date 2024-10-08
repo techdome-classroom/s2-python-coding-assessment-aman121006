3c3567cce1366cccfcaf17c8c0056db66ecd7a1c
Aman Kumar <amankumarsharma7000@gmail.com>
2024-10-08 13:58:48 +0530

    Done

--- program1.py
+++ program1.py
@@ -1,20 +1,22 @@
 class Solution(object):
     def isValid(self, s):
-        """
-        :type s: str
-        :rtype: bool
-        """
-        pass
-
-
-
-
-
-
-
-    
-
-
-
-  
-
+        # Dictionary to map closing brackets to corresponding opening brackets
+        bracket_map = {')': '(', '}': '{', ']': '['}
+        # Stack to keep track of opening brackets
+        stack = []
+        
+        # Iterate through each character in the string
+        for char in s:
+            # If it's a closing bracket
+            if char in bracket_map:
+                # Pop from stack if not empty, else assign a dummy value
+                top_element = stack.pop() if stack else '#'
+                # Check if the popped element is the correct opening bracket
+                if bracket_map[char] != top_element:
+                    return False
+            else:
+                # If it's an opening bracket, push onto the stack
+                stack.append(char)
+        
+        # Check if the stack is empty
+        return not stack
--- program2.py
+++ program2.py
@@ -1,10 +1,17 @@
 class Solution(object):
     def romanToInt(self, s):
-        """
-        :type s: str
-        :rtype: int
-        """
-        pass
-
-
-
+        # Dictionary to map Roman numerals to integers
+        roman_map = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+        # Variable to store the integer result
+        result = 0
+        
+        # Traverse through the string
+        for i in range(len(s)):
+            # If the current numeral is less than the next numeral, subtract its value
+            if i < len(s) - 1 and roman_map[s[i]] < roman_map[s[i + 1]]:
+                result -= roman_map[s[i]]
+            else:
+                # Otherwise, add its value
+                result += roman_map[s[i]]
+        
+        return result
